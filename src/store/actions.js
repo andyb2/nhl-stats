89d295bad4e780081b00a29c595860aa9e624acc
AMB <a.michael.boyle@gmail.com>
2021-10-13 13:47:30 -0400

    COMPONENT_TOGGLE added. When a widget is clicked it will toggle the corresponding component and render the more in-depth data.

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,17 @@
-import { setInitialState } from "./reducerFunctions";
+import {
+    setInitialState,
+    setActiveComponent,
+} from "./reducerFunctions";
 
 const SET_NHL_DATA_INITIAL_STATE = 'SET_NHL_DATA_INITIAL_STATE';
+const COMPONENT_TOGGLE = 'COMPONENT_TOGGLE'
+
+export const toggleComponents = (activeComponent) => {
+    return {
+        type: COMPONENT_TOGGLE,
+        payload: activeComponent,
+    }
+}
 
 export const setNhlDataInitialState = (nhlData) => {
     return {
@@ -13,6 +24,8 @@ const reducer = (state = [], action) => {
     switch (action.type) {
         case SET_NHL_DATA_INITIAL_STATE:
             return setInitialState(state, action.payload);
+        case COMPONENT_TOGGLE:
+            return setActiveComponent(state, action.payload);
         default:
             return state;
     }
