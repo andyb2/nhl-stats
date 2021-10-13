77cca54ff77a999050b4cd32795794f947901d81
AMB <a.michael.boyle@gmail.com>
2021-10-13 13:42:38 -0400

    mapStateToProps created to get the state of active components. When the standings widget is clicked the toggleComponents function is called to pass a boolean to the store. The widget will visibility will be set to false so the child component will be rendered

--- src/Widgets/WStandings/WStandings.js
+++ src/Widgets/WStandings/WStandings.js
@@ -1,36 +1,69 @@
 import { connect } from 'react-redux'
 import './WStandings.css'
-import Sample from '../../tempJSON/data'
+// import Sample from '../../tempJSON/data'
+import { toggleComponents } from '../../store/actions'
+import { useState } from 'react'
 
 const WStandings = (props) => {
 
-    console.log(props)
+
+    // NHL data drilling
+    const { standings } = props
+    const { data } = standings
+    const { records } = data
+
+    // Component render toggle
+    const { activeComponent } = props
+    const { WStandings } = activeComponent
+    console.log(`my props`, activeComponent)
+
+    const renderFullStats = () => {
+        props.toggleComponents({ WStandings: !WStandings })
+    }
+
     return (
-        <div className="standingsWidget">
+        <div className="standingsWidget" onClick={renderFullStats}>
             <h1 className="widgetHeader">Standings</h1>
             <div className="widget">
-                <table className="WstandingsTable">
-                    <thead className="WStandingsHeader">
-                        <tr>
-                            <th>West</th>
-                            <th>Points</th>
-                        </tr>
-                    </thead>
-                    {Sample.data.records[0].teamRecords.map(teamW => {
-                        return (
-                            <tbody>
-                                <tr className="WteamCard">
-                                    <td>{teamW.team.name}</td>
-                                    <td>92</td>
+                {records.map((teamStandings, idx) => {
+                    return (
+                        <table className="WstandingsTable" key={idx}>
+                            <thead className="WStandingsHeader">
+                                <tr>
+                                    <th>{teamStandings.division.name}</th>
+                                    <th>Points</th>
                                 </tr>
-                            </tbody>
-                        )
-                    })}
-                </table>
+                            </thead>
+                            {teamStandings.teamRecords.map((teamW, idx) => {
+                                return (
+                                    <tbody key={idx}>
+                                        <tr className="WteamCard">
+                                            <td>{teamW.team.name}</td>
+                                            <td>{teamW.points}</td>
+                                        </tr>
+                                    </tbody>
+                                )
+                            })}
+                        </table>
+                    )
+                })
+                }
             </div>
 
         </div>
     )
 }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        toggleComponents: (WStandings) => {
+            dispatch(toggleComponents(WStandings))
+        }
+    }
+}
 
-export default WStandings
+const mapStateToProps = (state) => {
+    return {
+        activeComponent: state.activeComponent
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(WStandings)
