515431ae87bd55f6529ca86de34a50c2b95013a2
AMB <a.michael.boyle@gmail.com>
2021-10-15 20:26:08 -0400

    setActiveComponent function created.. when a widget is clicked on the fullview will display its corresponding component. This function toggles if the component is active(true) or not active(false)

--- src/store/reducerFunctions.js
+++ src/store/reducerFunctions.js
@@ -3,7 +3,11 @@ export const setInitialState = (state, payload) => {
 }
 
 export const setActiveComponent = (state, payload) => {
+    console.log(payload)
     const stateCopy = { ...state }
     stateCopy.activeComponent[`${Object.keys(payload)[0]}`] = !state.activeComponent[`${Object.keys(payload)[0]}`]
+    stateCopy.activeComponent[`${Object.keys(payload)[1]}`] = !state.activeComponent[`${Object.keys(payload)[1]}`]
+    // console.log(`TEST`, stateCopy.activeComponent, `payload`, stateCopy.activeComponent[`${Object.keys(payload)[1]}`])
+
     return stateCopy
 }
