91de97c8a5ba981ab9b919b645209b39c969b203
AMB <a.michael.boyle@gmail.com>
2021-10-15 20:29:34 -0400

    teamAndPlayerData function was created to request team data. the team id is then mapped into another get request to receive roster information. This roster contains all players on a specific team. The players id will then be used to request player stats from another get request.

--- src/store/thunkCreators.js
+++ src/store/thunkCreators.js
@@ -3,8 +3,24 @@ import { setNhlDataInitialState } from "./actions";
 
 export const nhlDataFetch = () => async (dispatch) => {
     try {
-        const nhlData = await axios.get('https://statsapi.web.nhl.com/api/v1/standings')
+        const nhlData = await axios.get('https://statsapi.web.nhl.com/api/v1/standings');
         dispatch(setNhlDataInitialState(nhlData))
+    } catch (error) {
+        console.error(error)
+    }
+}
+// const test = await axios.get('https://statsapi.web.nhl.com/api/v1/teams/2/roster');
+export const teamAndPlayerData = () => async (dispatch) => {
+    try {
+        const storedPlayerData = [];
+        const teamList = await axios.get(`https://statsapi.web.nhl.com/api/v1/teams/`);
+
+        const playerIdFetch = teamList.data.teams.map(async (team) => {
+            const tempData = await axios.get(`https://statsapi.web.nhl.com/api/v1/teams/${team.id}/roster`);
+            storedPlayerData.push(tempData)
+        });
+
+        console.log(`[STORED DATA]`, storedPlayerData)
     } catch (error) {
         console.error(error)
     }
