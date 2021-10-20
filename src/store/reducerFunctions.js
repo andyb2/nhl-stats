4017f3d32eb291f86a7ecc3a09932fc373c59d01
AMB <a.michael.boyle@gmail.com>
2021-10-19 21:14:37 -0400

    setTeamData function receives the old state and payload and updates to a new state containing the payload

--- src/store/reducerFunctions.js
+++ src/store/reducerFunctions.js
@@ -3,11 +3,23 @@ export const setInitialState = (state, payload) => {
 }
 
 export const setActiveComponent = (state, payload) => {
-    console.log(payload)
-    const stateCopy = { ...state }
-    stateCopy.activeComponent[`${Object.keys(payload)[0]}`] = !state.activeComponent[`${Object.keys(payload)[0]}`]
-    stateCopy.activeComponent[`${Object.keys(payload)[1]}`] = !state.activeComponent[`${Object.keys(payload)[1]}`]
-    // console.log(`TEST`, stateCopy.activeComponent, `payload`, stateCopy.activeComponent[`${Object.keys(payload)[1]}`])
+    // console.log(`TEMAS`, Object.keys(payload).length)
 
-    return stateCopy
+    const stateCopy = { ...state }
+    if (Object.keys(payload).length === 1) {
+        stateCopy.activeComponent[`${Object.keys(payload)[0]}`] = !stateCopy.activeComponent[`${Object.keys(payload)[0]}`]
+        console.log(`the payload`, state.activeComponent[`${Object.keys(payload)}`])
+        return stateCopy
+    } else {
+
+        stateCopy.activeComponent[`${Object.keys(payload)[0]}`] = !stateCopy.activeComponent[`${Object.keys(payload)[0]}`]
+        stateCopy.activeComponent[`${Object.keys(payload)[1]}`] = !stateCopy.activeComponent[`${Object.keys(payload)[1]}`]
+        // console.log(`TEST`, stateCopy.activeComponent, `payload`, stateCopy.activeComponent[`${Object.keys(payload)[1]}`])
+        return stateCopy
+    }
+}
+
+export const setTeamData = (state, payload) => {
+    const stateCopy = { ...state }
+    return { ...stateCopy, teams: payload }
 }
