8a334401b46b54acf5294af00264607ecd0aa100
AMB <a.michael.boyle@gmail.com>
2021-10-19 21:13:44 -0400

    teamInfo function receives a get request for a list of teams. This data is sorted alphabetically and passed to global state

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,12 @@
 import {
     setInitialState,
     setActiveComponent,
+    setTeamData,
 } from "./reducerFunctions";
 
 const SET_NHL_DATA_INITIAL_STATE = 'SET_NHL_DATA_INITIAL_STATE';
-const COMPONENT_TOGGLE = 'COMPONENT_TOGGLE'
+const COMPONENT_TOGGLE = 'COMPONENT_TOGGLE';
+const SET_TEAM_DATA = 'SET_TEAM_DATA';
 
 export const toggleComponents = (activeComponent) => {
     return {
@@ -20,12 +22,21 @@ export const setNhlDataInitialState = (nhlData) => {
     }
 }
 
+export const teamInfo = (teamSort) => {
+    return {
+        type: SET_TEAM_DATA,
+        payload: teamSort
+    }
+}
+
 const reducer = (state = [], action) => {
     switch (action.type) {
         case SET_NHL_DATA_INITIAL_STATE:
             return setInitialState(state, action.payload);
         case COMPONENT_TOGGLE:
             return setActiveComponent(state, action.payload);
+        case SET_TEAM_DATA:
+            return setTeamData(state, action.payload);
         default:
             return state;
     }
