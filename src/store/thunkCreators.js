c232c00e22b2092b8db49c409b7f87291e9669e6
AMB <a.michael.boyle@gmail.com>
2021-10-21 16:05:59 -0400

    selectedTeam function receives the selected team info when a user clicks on a team in the sidebar. the nhlteam parameter contains a link to retrieve more information from a get request. This link is used in the nhlTeamRoster get request to update the global state with the selected teams roster

--- src/store/thunkCreators.js
+++ src/store/thunkCreators.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { setNhlDataInitialState, teamInfo } from "./actions";
+import { setNhlDataInitialState, teamInfo, selectedNhlTeam } from "./actions";
 
 export const nhlDataFetch = () => async (dispatch) => {
     try {
@@ -27,24 +27,18 @@ export const teamFetch = () => async (dispatch) => {
     }
 }
 
+export const selectedTeam = (nhlTeam) => async (dispatch) => {
+    try {
+        const { link } = nhlTeam
+        const nhlTeamRoster = await axios.get(`https://statsapi.web.nhl.com/${link}/roster`);
+        const test = await axios.get('https://statsapi.web.nhl.com/api/v1/people/8479368/stats/?stats=yearByYear')
+        console.log(`stat`, test)
+        // const playerStats = nhlTeamRoster.data.roster.map(async(playerId) => {
+        //     const playerStat = await 
+        // })
 
-// export const teamAndPlayerData = () => async (dispatch) => {
-//     try {
-//         const playersId = [];
-//         const storedPlayerData = [];
-//         const teamList = await axios.get(`https://statsapi.web.nhl.com/api/v1/teams/`);
-
-//         const player = teamList.data.teams.map(async (team) => {
-//             const tempData = await axios.get(`https://statsapi.web.nhl.com/api/v1/teams/${team.id}/roster/`);
-//             // const test = tempData.data.roster.map(data => {
-//             //     storedPlayerData.push(data.person.id)
-//             // })
-//             storedPlayerData.push(...tempData.data.roster);
-//         });
-//         console.log(storedPlayerData)
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
-
-// ?teamId=4,5,29
+        dispatch(selectedNhlTeam(nhlTeamRoster, nhlTeam))
+    } catch (error) {
+        console.error(error)
+    }
+}
