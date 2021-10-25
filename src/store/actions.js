3970f180385fe4bd2b0ac7de189cc916cb87245c
AMB <a.michael.boyle@gmail.com>
2021-10-25 11:38:35 -0400

    individual player states action created. This action will be used by a player search bar that will be added

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,12 +3,14 @@ import {
     setActiveComponent,
     setTeamData,
     teamRoster,
+    playerSearchedStats,
 } from "./reducerFunctions";
 
 const SET_NHL_DATA_INITIAL_STATE = 'SET_NHL_DATA_INITIAL_STATE';
 const COMPONENT_TOGGLE = 'COMPONENT_TOGGLE';
 const SET_TEAM_DATA = 'SET_TEAM_DATA';
-const SET_TEAM_ROSTER = 'SET_TEAM_ROSTER'
+const SET_TEAM_ROSTER = 'SET_TEAM_ROSTER';
+const SET_PLAYER_DATA = 'SET_PLAYER_DATA';
 
 export const toggleComponents = (activeComponent) => {
     return {
@@ -38,6 +40,13 @@ export const selectedNhlTeam = (teamRoster, nhlTeam) => {
     }
 }
 
+export const playerInfo = (stats) => {
+    return {
+        type: SET_PLAYER_DATA,
+        payload: stats
+    }
+}
+
 const reducer = (state = [], action) => {
     switch (action.type) {
         case SET_NHL_DATA_INITIAL_STATE:
@@ -47,7 +56,9 @@ const reducer = (state = [], action) => {
         case SET_TEAM_DATA:
             return setTeamData(state, action.payload);
         case SET_TEAM_ROSTER:
-            return teamRoster(state, action.payload)
+            return teamRoster(state, action.payload);
+        case SET_PLAYER_DATA:
+            return playerSearchedStats(state, action.payload);
         default:
             return state;
     }
