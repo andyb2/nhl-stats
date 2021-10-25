39c26a984e9a3bcbfdbe5b0e058fa62749a7ee3a
AMB <a.michael.boyle@gmail.com>
2021-10-25 11:36:46 -0400

    When a team is selected from the sidebar the roster now shows two tables. The first table is for forwards and defence. The second table shows goalies. Two tables needed to be created because forwards/defence have different stats compared to goalies

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,11 +1,12 @@
 import { connect } from "react-redux";
-
+import { playerSearchedStats } from "../../store/reducerFunctions";
+import { playerStats } from "../../store/thunkCreators";
 
 const Team = (props) => {
 
     const { selectedTeam } = props;
     const { nhlTeam } = selectedTeam
-    const { roster } = selectedTeam
+    const { teamRoster } = selectedTeam
     console.log(`TEAM PROP`, selectedTeam)
 
     const playerSelect = async (playerId) => {
@@ -29,27 +30,61 @@ const Team = (props) => {
                         <th>POS</th>
                         <th>Jersey #</th>
                         <th>Player Name</th>
+                        <th>GP</th>
+                        <th>Goals</th>
                     </tr>
                 </thead>
 
-                {roster.map(players => {
-                    return (
-                        <tbody>
-                            <tr>
-                                <td>{players.position.code}</td>
-                                <td>{players.jerseyNumber}</td>
-                                <td onClick={() => playerSelect(players.person.id)}>{players.person.fullName}</td>
-                            </tr>
-                        </tbody>
-                    )
-                })
-                }
+                {teamRoster.map(players => {
+                    if (players.name.position.code !== 'G') {
+                        return (
+                            <tbody key={players.name.person.fullName}>
+                                <tr>
+                                    <td>{players.name.position.code}</td>
+                                    <td>{players.name.jerseyNumber}</td>
+                                    <td onClick={() => playerSelect(players.name.person.id)}>{players.name.person.fullName}</td>
+                                    <td>{players.stats[0].splits.length !== 0 ? players.stats[0].splits[0].stat.games : 0}</td>
+                                    <td>{players.stats[0].splits.length !== 0 ? players.stats[0].splits[0].stat.goals : 0}</td>
+                                </tr>
+                            </tbody>
+                        )
+                    }
+                })}
+            </table>
+            <table>
+                <thead>
+                    <tr>
+                        <th>POS</th>
+                        <th>Jersey #</th>
+                        <th>Player Name</th>
+                        <th>GP</th>
+                        <th>GAA</th>
+                        <th>SV %</th>
+                    </tr>
+                </thead>
+                {teamRoster.map(goalies => {
+                    if (goalies.name.position.code === 'G') {
+                        return (
+                            <tbody key={goalies.name.person.fullName}>
+                                <tr>
+                                    <td>{goalies.name.position.code}</td>
+                                    <td>{goalies.name.jerseyNumber}</td>
+                                    <td onClick={() => playerSelect(goalies.name.person.id)}>{goalies.name.person.fullName}</td>
+                                    <td>{goalies.stats[0].splits[0].stat.games}</td>
+                                    <td>{goalies.stats[0].splits[0].stat.goalAgainstAverage}</td>
+                                    <td>{goalies.stats[0].splits[0].stat.savePercentage}</td>
+                                    <td>{console.log(goalies)}</td>
+                                </tr>
+                            </tbody>
+                        )
+                    }
+                })}
             </table>
 
         </div>
     )
 }
-
+// Object.keys(players.stats[0].splits).length - 1
 const mapDispatchToProps = (dispatch) => {
     return {
         playerStats: (playerId) => {
